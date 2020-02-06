1e119fe00da5d3edb257d449c47b29bc2956406e
Paul Stanley <paulstanley@protonmail.com>
2020-02-06 16:16:51 +0800

    Added package tests

---
+++ src/ByteDev.Collections.PackageTester.NetCore/Tests.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using NUnit.Framework;
+
+namespace ByteDev.Collections.PackageTester.NetCore
+{
+    [TestFixture]
+    public class Tests
+    {
+        [Test]
+        public void RunTests()
+        {
+            var sut = new List<int> { 1,2,3,4,5,6,7,8,9,10 };
+
+            var result = sut.Tenth();
+
+            Assert.That(result, Is.EqualTo(10));
+        }
+    }
+}
